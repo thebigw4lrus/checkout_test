0c1258a75e5421a543b4a05c995aad19de9818dd
jcontreras002 <jcontreras000@gmail.com>
2013-02-19 22:47:04 -0430

    Test Optimized

--- unit_test/checkout_acceptance_test_cases.rb
+++ unit_test/checkout_acceptance_test_cases.rb
@@ -15,14 +15,16 @@ class CheckoutAcceptanceTestCases < Test::Unit::TestCase
   PRODUCT_FR1 = Product.new("FR1", 3.11)
   PRODUCT_SR1 = Product.new("SR1", 5.00)
   PRODUCT_CF1 = Product.new("CF1", 11.23) 
-  
-  
+  CHECKOUT =  Checkout.new(DiscountBOGAFRule.new(PRODUCT_FR1), 
+                           DiscountBulkPurchaseRule.new(PRODUCT_SR1,3, 4.50),
+                           ProductRule.new(PRODUCT_FR1), 
+                           ProductRule.new(PRODUCT_SR1),
+                           ProductRule.new(PRODUCT_CF1))
+  #I don't know if i missed something, but 
+  #according the rules required, this total 
+  #should be 19.34.
   def test_case_fr1_sr1_fr1_cf1
-    checkout = Checkout.new(DiscountBOGAFRule.new(PRODUCT_FR1), 
-                            DiscountBulkPurchaseRule.new(PRODUCT_SR1,3, 4.50),
-                            ProductRule.new(PRODUCT_FR1), 
-                            ProductRule.new(PRODUCT_SR1),
-                            ProductRule.new(PRODUCT_CF1))
+    checkout = CHECKOUT
     checkout.scan "FR1"
     checkout.scan "SR1"
     checkout.scan "FR1"
@@ -31,22 +33,14 @@ class CheckoutAcceptanceTestCases < Test::Unit::TestCase
   end
   
   def test_case_fr1_fr1
-    checkout = Checkout.new(DiscountBOGAFRule.new(PRODUCT_FR1), 
-                            DiscountBulkPurchaseRule.new(PRODUCT_SR1,3, 4.50),
-                            ProductRule.new(PRODUCT_FR1), 
-                            ProductRule.new(PRODUCT_SR1),
-                            ProductRule.new(PRODUCT_CF1))
+    checkout = CHECKOUT
     checkout.scan "FR1"
     checkout.scan "FR1"
     assert_equal 3.11, checkout.total
   end
 
   def test_case_sr1_sr1_fr1_sr1
-    checkout = Checkout.new(DiscountBOGAFRule.new(PRODUCT_FR1), 
-                            DiscountBulkPurchaseRule.new(PRODUCT_SR1,3, 4.50),
-                            ProductRule.new(PRODUCT_FR1), 
-                            ProductRule.new(PRODUCT_SR1),
-                            ProductRule.new(PRODUCT_CF1))
+    checkout = CHECKOUT
     checkout.scan "SR1"
     checkout.scan "SR1"
     checkout.scan "FR1"
